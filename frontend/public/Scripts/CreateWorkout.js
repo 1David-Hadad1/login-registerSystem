2c542adc8bf7f5a715f6210c9db9afe9396b243a
1David-Hadad1 <dh32a32@gmail.com>
2026-05-16 03:30:44 +0300

    fixed bug and add remove btn

--- frontend/public/Scripts/CreateWorkout.js
+++ frontend/public/Scripts/CreateWorkout.js
@@ -7,6 +7,13 @@ document.getElementById('addExercise').addEventListener("click", () =>{
          <input type="text" name="exercise[]" placeholder="שם של התרגיל">
          <input type="number" name="weight[]" placeholder="משקל">
          <input type="number" name="reps[]" placeholder="חזרות">
+         <button type="button" class="delete-btn">מחק</button>
       </div>
    `
+});
+
+exerciseCon.addEventListener("click", (e) =>{
+   if(e.target.classList.contains("delete-btn")){
+      e.target.parentElement.remove();
+   }
 });
