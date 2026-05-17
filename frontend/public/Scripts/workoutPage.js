73ba6a4be34aa44e4cd106da9b1e627ca54c7b74
1David-Hadad1 <dh32a32@gmail.com>
2026-05-18 01:13:26 +0300

    fixed

--- frontend/public/Scripts/workoutPage.js
+++ frontend/public/Scripts/workoutPage.js
@@ -6,13 +6,21 @@ addSetBtn.forEach(btn => {
 
    btn.addEventListener("click", () =>{
       const setRow = document.createElement("div");
+      
       setRow.innerHTML += `
          <div class="set-row">
             <input type="number" name="weight" placeholder="weight" required>
             <input type="number" name="reps" placeholder="reps" required>
+            <button type="button" class="delete-btn">מחק תרגיל</button>
          </div>
       `
 
       setDiv.appendChild(setRow);
+   });
+
+   setDiv.addEventListener("click", (e) =>{
+      if(e.target.classList.contains("delete-btn")){
+         e.target.parentElement.remove();
+      }
    });
 });
